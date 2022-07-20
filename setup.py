d1e2dc0a1f3b4d3f116fa9b0a4a4a5343bf97145
MosesDastmard <dastmard68@gmail.com>
2022-07-20 21:40:49 +0200

    add the ability to wait for a busy file till it gets idle to perform a block of code on the given file or directory

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="flagify",
-    version="1.0.1",
+    version="1.1.0",
     author="Moses Dastmard",
     description="put/remove flags for files and folders",
 )
